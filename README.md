6a61ea9701b8eb323b481ee89dd8e043c25b7b1e
gulzerr <gulzer.deb@gmail.com>
2024-08-01 22:57:16 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sticky Notes React
+
+Drag & Drop sticky notes App built with React JS and BunJs.
+
+<img src="https://sticky-fcc.vercel.app/demo.gif"/>
+
+### Features
+
+- Drag and Drop notes anywhere on screen
+- Save note data, position and color in database
+- Change note color at anytime
+- Autogrow note size as data is input
+- Autosave notes as you add data.
+
+### Setup instructions
+
+- Clone repo: `git clone <REPO URL>`
+- Install dependencies and run server: `cd reponame` + `npm i` + `npm run dev`
+- Setup BunJs backend with database (details will be given later)
+- Create `.env` file (use `.env.example` as a reference) and update appwrite credentials
+
+### BunJs backend
+
+(Comming soon....)
---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,81 @@
+import { useRef, useEffect, useState } from "react";
+import Trash from "../icons/Trasch";
+import { setNewOffset, autoGrow, setZIndex } from "../utils";
+
+const NoteCard = ({ note }) => {
+  const body = JSON.parse(note.body);
+  const [position, setPosition] = useState(JSON.parse(note.position));
+  const colors = JSON.parse(note.colors);
+
+  let mouseStartPos = { x: 0, y: 0 };
+  const cardRef = useRef(null);
+
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+
+    setZIndex(cardRef.current);
+  };
+
+  const mouseMove = (e) => {
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+
+    setPosition(newPosition);
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
+  return (
+    <div
+      ref={cardRef}
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        onMouseDown={mouseDown}
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
+          onFocus={() => {
+            setZIndex(cardRef.current);
+          }}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
